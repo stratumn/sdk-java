b3cddd317f2f880d4d2edc4e96b60a25177bf921
Mourad BEJI <mourad.beji@stratumn.com>
2019-07-25 15:02:09 +0200

    add proxy

--- src/main/java/com/stratumn/sdk/SdkOptions.java
+++ src/main/java/com/stratumn/sdk/SdkOptions.java
@@ -1,11 +1,16 @@
 package com.stratumn.sdk;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 import com.stratumn.sdk.model.client.*;
 
 public class SdkOptions {
   String workflowId;
   Secret secret;
   Endpoints endpoints;
+
+  Proxy proxy;
 
   public SdkOptions(String workflowId, Secret secret) {
     this.workflowId = workflowId;
@@ -23,5 +28,16 @@ public class SdkOptions {
     this.workflowId = workflowId;
     this.secret = secret;
     this.endpoints = endpoints;
+  }
+
+  public void setProxy(String host, int port) throws IllegalArgumentException {
+    if (host == null) {
+      throw new IllegalArgumentException("host cannot be null in proxy");
+    }
+    if (port == 0) {
+      throw new IllegalArgumentException("port cannot be 0 in proxy");
+    }
+
+    this.proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port));
   }
 }
