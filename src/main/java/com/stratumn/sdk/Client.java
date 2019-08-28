8f03ccdfd2fa92ac340dfa8e0906f3bb97dbb0a8
step <stepstpdev@gmail.com>
2019-08-28 17:55:22 +0300

    Merged GraphQLRequest  low level methods int Client

--- src/main/java/com/stratumn/sdk/Client.java
+++ src/main/java/com/stratumn/sdk/Client.java
@@ -56,21 +56,26 @@ import com.stratumn.sdk.model.client.Secret;
 import com.stratumn.sdk.model.client.Service;
 import com.stratumn.sdk.model.file.FileInfo;
 import com.stratumn.sdk.model.file.MediaRecord;
-
 /**
- * A wrapper client to handle communication with account, trace and media via
- * REST and GraphQL
+ * A wrapper client to handle communication
+ * with account, trace and media via REST and GraphQL
  *
- * The Client will handle (re-)authentication if a token is not present yet or
- * expired.
+ * The Client will handle (re-)authentication if
+ * a token is not present yet or expired.
  *
- * The Client exposes 3 main methods: - get - post - graphql
+ * The Client exposes 3 main methods:
+ * - get
+ * - post
+ * - graphql 
  */
-public class Client {
+public class Client
+{
    /**
-    * The default GraphQL options: - retry once
+    * The default GraphQL options:
+    * - retry once
     */
-   private static final GraphQLOptions DefaultGraphQLOptions = new GraphQLOptions(1);
+   private static final GraphQLOptions  DefaultGraphQLOptions =new GraphQLOptions(1);
+  
 
    private static final FetchOptions DefaultFetchOptions = new FetchOptions();
 
@@ -93,10 +98,10 @@ public class Client {
 
    /***
     * Constructs a new instance of the Client
-    * 
     * @param opts
     */
-   public Client(ClientOptions opts) {
+   public Client(ClientOptions opts)
+   {
       this.endpoints = Helpers.makeEndpoints(opts.getEndpoints());
       this.proxy = opts.getProxy();
 
@@ -130,13 +135,15 @@ public class Client {
 
    }
 
+ 
    /**
     * Compute the bearer Authorization header of format "Bearer my_token". If the
     * token is undefined, the return header is an empty string "".
     *
     * @param token optional token to be used
     */
-   private String makeAuthorizationHeader(String token) {
+   private String makeAuthorizationHeader(String token)
+   {
       return (token != null) ? "Bearer " + token : "";
    }
 
@@ -149,7 +156,7 @@ public class Client {
     * @param opts           optional options
     * @param opts.authToken optional token to be used
     * @param opts.skipAuth  optional flag to bypass authentication
-    * @throws TraceSdkException
+    * @throws TraceSdkException  
     */
    private String getAuthorizationHeader(FetchOptions opts) throws TraceSdkException {
       if (opts != null) {
@@ -168,14 +175,16 @@ public class Client {
     * 
     * @param token the new token
     */
-   private void setToken(String token) {
+   private void setToken(String token)
+   {
       this.token = token;
    }
 
    /**
     * To clear the existing token
     */
-   private void clearToken() {
+   private void clearToken()
+   {
       this.token = null;
    }
 
@@ -197,10 +206,11 @@ public class Client {
                || (200 <= con.getResponseCode() && con.getResponseCode() <= 299);
          if (!ok) {
             // if 401 and retry > 0 then we can retry
-            if (status == 401 && retry > 0) {
+            if(status == 401 && retry > 0)
+            {
                // unauthenticated request might be because token expired
                // clear token and retry
-               this.clearToken();
+               this.clearToken(); 
                return this.fetch(request, --retry);
             }
 
@@ -225,9 +235,10 @@ public class Client {
     * Authenticate using a signed message via the GET /login route.
     *
     * @param key the signing private key in clear text used to log in
-    * @throws Exception
+    * @throws Exception  
     */
-   private void loginWithSigningPrivateKey(String pemPrivateKey) throws Exception {
+   private void loginWithSigningPrivateKey(String pemPrivateKey) throws Exception
+   {
 
       PrivateKey privateKey = CryptoUtils.decodePrivateKey(pemPrivateKey);
 
@@ -243,10 +254,11 @@ public class Client {
     * Authenticates using a user's credentials via the POST /login route.
     *
     * @param email    the email of the user
-    * @param password the password of the user
-    * @throws TraceSdkException
+    * @param password the password of the user 
+    * @throws TraceSdkException 
     */
-   private void loginWithCredentials(String email, String password) throws TraceSdkException {
+   private void loginWithCredentials(String email, String password) throws TraceSdkException 
+   {
       // get the user salt first
       // use skipAuth = true to bypass authentication
       // GET /salt is a public route!
@@ -278,45 +290,58 @@ public class Client {
     * Authenticates using a valid secret. Supported secret types are: -
     * CredentialSecret -> via email+password - PrivateKeySecret -> via signed
     * message
-    * 
-    * @throws TraceSdkException
+    * @throws TraceSdkException  
     */
-   private synchronized void login() throws TraceSdkException {
+   private synchronized void login() throws TraceSdkException
+   {
 
       // if another concurrent execution has already
       // done the job, then release and return, nothing to do.
-      if (this.token != null)
-         return;
+      if(this.token != null) return;
 
       // otherwise do the job...
-      if (Secret.isCredentialSecret(this.secret)) {
+      if(Secret.isCredentialSecret(this.secret))
+      {
          // the CredentialSecret case
          final String email = ((CredentialSecret) this.secret).getEmail();
          final String password = ((CredentialSecret) this.secret).getPassword();
-         try {
+         try { 
             this.loginWithCredentials(email, password);
-         } catch (TraceSdkException e) {
-            throw new TraceSdkException("Login with email password failed.", e);
+         }catch (TraceSdkException e)
+         {
+            throw new TraceSdkException("Login with email password failed.",e);
          }
-      } else if (Secret.isPrivateKeySecret(this.secret)) {
-         // the PrivateKeySecret case
-         final String privateKey = ((PrivateKeySecret) this.secret).getPrivateKey();
-         try {
-            this.loginWithSigningPrivateKey(privateKey);
-         } catch (Exception e) {
-            throw new TraceSdkException("Login with private key failed", e);
-         }
-      } else if (Secret.isProtectedKeySecret(this.secret)) {
-         // the ProtectedKeySecret case
-         // not handled yet
-         throw new TraceSdkException("Authentication via password protected key is not handled");
-      } else {
-         // Unknown case
-         throw new TraceSdkException("The provided secret does not have the right format");
       }
+      else
+         if(Secret.isPrivateKeySecret(this.secret))
+         {
+            // the PrivateKeySecret case
+            final String privateKey = ((PrivateKeySecret) this.secret).getPrivateKey();
+            try
+            {
+               this.loginWithSigningPrivateKey(privateKey);
+            }
+            catch(Exception e)
+            {
+               throw new TraceSdkException("Login with private key failed",e);
+            } 
+         }
+         else
+            if(Secret.isProtectedKeySecret(this.secret))
+            {
+               // the ProtectedKeySecret case
+               // not handled yet
+               throw new TraceSdkException("Authentication via password protected key is not handled");
+            }
+            else
+            {
+               // Unknown case
+               throw new TraceSdkException("The provided secret does not have the right format");
+            }
 
    }
 
+  
    /**
     * Executes a POST query on a target service.
     *
@@ -333,10 +358,10 @@ public class Client {
          if (opts == null)
             opts = DefaultFetchOptions;
 
-         // References: https://www.baeldung.com/java-http-request
-         // https://juffalow.com/java/how-to-send-http-get-post-request-in-java
-         String path = this.endpoints.getEndpoint(service) + '/' + route;
-         URL url = new URL(path);
+      // References: https://www.baeldung.com/java-http-request
+      // https://juffalow.com/java/how-to-send-http-get-post-request-in-java
+      String path = this.endpoints.getEndpoint(service) + '/' + route;
+      URL url = new URL(path);
 
          HttpURLConnection con;
          if (this.proxy != null) {
@@ -345,21 +370,22 @@ public class Client {
             con = (HttpURLConnection) url.openConnection();
          }
 
-         con.setRequestMethod("POST");
-         con.setRequestProperty("Content-Type", "application/json");
-         con.setRequestProperty("Accept", "application/json");
-         con.setRequestProperty("Authorization", this.getAuthorizationHeader(opts));
-         con.setDoOutput(true);
+      con.setRequestMethod("POST");
+      con.setRequestProperty("Content-Type", "application/json");
+      con.setRequestProperty("Accept", "application/json");
+      con.setRequestProperty("Authorization", this.getAuthorizationHeader(opts)); 
+      con.setDoOutput(true);
+      
+      HttpHelpers request = new HttpHelpers(con);
+      request.setBody(body);
 
-         HttpHelpers request = new HttpHelpers(con);
-         request.setBody(body);
+      Integer retry = opts.getRetry();
 
-         Integer retry = opts.getRetry();
-
-         // delegate to fetch wrapper
-         return this.fetch(request, retry);
-
-      } catch (Exception e) {
+      // delegate to fetch wrapper
+      return this.fetch(request, retry);
+      
+      }catch (Exception e)
+      {
          throw new TraceSdkException("Error executing post request", e);
       }
    }
@@ -384,7 +410,8 @@ public class Client {
 
          String path = this.endpoints.getEndpoint(service) + '/' + route;
 
-         if (params != null) {
+         if(params != null)
+         {
             path += "?" + HttpHelpers.getParamsString(params);
          }
 
@@ -405,7 +432,9 @@ public class Client {
 
          // delegate to fetch wrapper
          return this.fetch(request, opts.getRetry());
-      } catch (Exception e) {
+      }
+      catch(Exception e)
+      {
          throw new TraceSdkException("Error executing get request", e);
       }
 
@@ -417,20 +446,24 @@ public class Client {
     * @param query     the graphql query / mutation
     * @param variables the graphql variables
     * @param opts      the graphql options
-    * @throws TraceSdkException
-    * 
+    * @throws TraceSdkException 
+ 
     */
-   public <T> T graphql(GraphQl.Query query, Map<String, Object> variables, GraphQLOptions opts, Class<T> tclass)
-         throws TraceSdkException {
+   public  <T>  T  graphql(GraphQl.Query query, Map<String, Object>  variables, GraphQLOptions opts, Class<T> tclass) throws TraceSdkException 
+   {
 
       String queryStr;
-      try {
+      try
+      {
          queryStr = query.loadQuery();
-      } catch (IOException e) {
-         throw new TraceSdkException("Error loading query", e);
       }
-      if (opts == null) {
-         opts = DefaultGraphQLOptions;
+      catch(IOException e)
+      {
+          throw new TraceSdkException("Error loading query",e);     
+      }
+      if(opts == null)
+      {
+         opts =  DefaultGraphQLOptions;
       }
       String gqlUrl = this.endpoints.getTrace() + "/graphql";
       GraphQlQuery topologyQuery = new GraphQlQuery(variables, queryStr);
@@ -438,23 +471,26 @@ public class Client {
       ResponseEntity<T> response = postForEntity(gqlUrl, topologyQuery, tclass);
       if (response.getStatusCode() == HttpStatus.OK) {
          // if the response is empty, throw.
-         if (!response.hasBody())
-            throw new TraceSdkException("The graphql response is empty.");
-      } else {
-         Integer retry = opts.getRetry();
-         // handle errors explicitly
-         // extract the status from the error response
+         if(!response.hasBody()) 
+            throw new TraceSdkException("The graphql response is empty."); 
+      }
+      else
+      {
+         Integer retry = opts.getRetry(); 
+         // handle errors explicitly 
+         // extract the status from the error response 
          // if 401 and retry > 0 then we can retry
-         if (response.getStatusCode() == HttpStatus.UNAUTHORIZED && retry > 0) {
+         if(response.getStatusCode() == HttpStatus.UNAUTHORIZED && retry > 0)
+         {
             // unauthenticated request might be because token expired
             // clear token and retry
-            this.clearToken();
-            opts.setRetry(--retry);
-            return this.graphql(query, variables, opts, tclass);
-         }
+            this.clearToken(); 
+            opts.setRetry(--retry); 
+            return this.graphql(query, variables, opts,tclass);
+         } 
          // otherwise rethrow
          throw new TraceSdkException(response.getBody().toString());
-      }
+      } 
       return response.getBody();
 
    }
@@ -476,14 +512,15 @@ public class Client {
 
       return mediaRecords;
    }
+   
 
    /**
     * Downloads a file corresponding to a media record.
     *
     * @param fileRecord the file record to download
     * @return the file data blob (Buffer)
-    * @throws TraceSdkException
-    * @throws HttpError
+    * @throws TraceSdkException 
+    * @throws HttpError 
     */
    public ByteBuffer downloadFile(FileRecord fileRecord) throws TraceSdkException, HttpError {
       String tokenResponse = this.get(Service.MEDIA, "files/" + fileRecord.getDigest() + "/info", null, null);
@@ -494,7 +531,8 @@ public class Client {
       final int BUFFER_SIZE = 4096;
 
       ByteBuffer byteBuffer = null;
-      try {
+      try
+      {
          URL url = new URL(downloadURL);
          HttpURLConnection httpConn;
          if (this.proxy != null) {
@@ -506,22 +544,26 @@ public class Client {
          int status = httpConn.getResponseCode();
          String statusText = httpConn.getResponseMessage();
          // always check HTTP response code first
-         if (status != HttpURLConnection.HTTP_OK) {
-            throw new HttpError(status, statusText);
+         if(status != HttpURLConnection.HTTP_OK)
+         {
+            throw new HttpError(status, statusText); 
          }
          // opens input stream from the HTTP connection
          InputStream inputStream = httpConn.getInputStream();
-         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();              
          byte[] buffer = new byte[BUFFER_SIZE];
          int bytesRead = 0;
-         while ((bytesRead = inputStream.read(buffer)) != -1) {
+         while((bytesRead = inputStream.read(buffer)) != -1)
+         {
             baos.write(buffer, 0, bytesRead);
-         }
-         baos.flush();
-         byteBuffer = ByteBuffer.wrap(baos.toByteArray());
+         }      
+         baos.flush();       
+         byteBuffer =  ByteBuffer.wrap( baos.toByteArray());
          baos.close();
          inputStream.close();
-      } catch (IOException e) {
+      }
+      catch( IOException e)
+      {
          throw new TraceSdkException(e);
       }
       return byteBuffer;
