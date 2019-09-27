559241aec8699b15bcc9dc88f07b93d9c46cd80d
Mourad BEJI <mourad.beji@stratumn.com>
2019-09-27 14:26:56 +0200

    fix timestamp adapter

--- src/main/java/com/stratumn/sdk/adapters/TimestampAdapter.java
+++ src/main/java/com/stratumn/sdk/adapters/TimestampAdapter.java
@@ -1,11 +1,9 @@
 package com.stratumn.sdk.adapters;
 
 import java.lang.reflect.Type;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -16,20 +14,16 @@ import com.google.gson.JsonParseException;
  */
 public class TimestampAdapter implements JsonDeserializer<Date> {
 
+
 	@Override
 	public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
-		String date = element.getAsString();
-		
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-		
 		try {
-			return formatter.parse(date);
-		} catch (ParseException pe) {
+			return new Gson().fromJson(element , Date.class); // default deserialization
+		} catch (Exception pe) {
             try {
             return new Date(element.getAsLong());
             } catch (Exception e){
-                System.err.printf("Failed to parse Date due to: %s\n", e.getMessage());
+                System.err.printf("Failed to parse Date due to: %s\n %s\n", pe.getMessage(), e.getMessage());
                 return null;
             }
 		}
